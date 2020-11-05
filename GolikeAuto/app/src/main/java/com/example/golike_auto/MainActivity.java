b4817df01d943a3066615b6dbbf8d92f56a3c9f5
LESYHUNG2612 <62929750+LESYHUNG2612@users.noreply.github.com>
2020-11-05 15:45:19 +0700

    taoProject

---
+++ GolikeAuto/app/src/main/java/com/example/golike_auto/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.golike_auto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
